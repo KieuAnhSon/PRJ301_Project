8563d406377ec40424850312396b877686c8bb7c
KieuAnhSon <105625231+KieuAnhSon@users.noreply.github.com>
2022-06-03 20:51:06 +0700

    Add some libs and database connection

---
+++ Project/src/java/Database/DBConnection.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Mountain
+ */
+public class DBConnection {
+
+    protected Connection connection;
+
+    public DBConnection() {
+        try {
+            String user = "sonka";
+            String pass = "kieuanhson02";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=PRJ301_Assignment";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
